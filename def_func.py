30832af1fe17133f5dd3c79be0eb92d5766aa4a8
Phong Chuc Vo <147295660+vchucphong1208@users.noreply.github.com>
2026-04-09 08:18:55 +0700

    Refactor df_dt to F1_n and dp_dt to F2_n

--- def_func.py
+++ def_func.py
@@ -35,60 +35,52 @@ def OmegaRabi(Y2, n_index, t, N):
     
     return (1 / h) * (term_pulse + coef * summation)
 
-def df_dt(p_array, n_index, t, N):
+
+def F1_n(Y2, n_index, t, N):
     """
-    Tính đạo hàm của hàm phân bố f_{j, k}(t) theo thời gian.
-    Vì phương trình của f_e và f_h giống hệt nhau ở vế phải, ta dùng chung 1 hàm.
-    
-    p_array: Mảng chứa toàn bộ các giá trị phân cực (p) đóng vai trò Y2.
-    n_index: Chỉ số k hiện tại (1-based index).
-    t: Thời gian hiện tại.
+    Tính F_{1,n} = complex(a, a)
+    Y2: Mảng chứa các giá trị Y_2 (đóng vai trò như độ phân cực p)
     """
     # Tính Omega Rabi hiệu dụng tại vị trí n_index
-    Omega_R = OmegaRabi(p_array, n_index, t, N)
+    Omega_R = OmegaRabi(Y2, n_index, t, N)
     
-    # Lấy giá trị p tại vị trí k hiện tại. 
-    # Do n_index tính từ 1 đến N, index trong mảng Python sẽ là n_index - 1
-    p_k = p_array[n_index - 1]
-    
-    # Công thức: -2 * Im[ Omega_R(t) * p*(t) ]
-    # np.conj() dùng để lấy số phức liên hợp (p*)
-    return -2 * np.imag(Omega_R * np.conj(p_k))
-
-
-def dp_dt(f_e_array, f_h_array, p_array, n_index, t, N, hbar, T2):
-    """
-    Tính đạo hàm của độ phân cực vi mô p_k(t) theo thời gian.
-    
-    f_e_array: Mảng phân bố electron (Y1 cho e_e).
-    f_h_array: Mảng phân bố lỗ trống (Y1 cho e_h).
-    p_array: Mảng độ phân cực (Y2 cho OmegaRabi).
-    hbar: Hằng số Planck rút gọn.
-    T2: Thời gian mất pha (Dephasing time).
-    """
-    # 1. Tính các năng lượng hiệu dụng e_e và e_h bằng hàm En của bạn
-    e_e = En(f_e_array, n_index, N)
-    e_h = En(f_h_array, n_index, N)
-    
-    # 2. Tính Omega Rabi hiệu dụng
-    Omega_R = OmegaRabi(p_array, n_index, t, N)
-    
-    # Lấy các giá trị cụ thể tại vị trí k hiện tại (index = n_index - 1)
+    # Lấy giá trị Y_{2,n} hiện tại (index = n_index - 1)
     idx = n_index - 1
-    p_k = p_array[idx]
-    f_e_k = f_e_array[idx]
-    f_h_k = f_h_array[idx]
+    Y2_n = Y2[idx]
     
-    # 3. Tính toán 3 thành phần của phương trình đạo hàm p_k
+    # Tính a = -2 * Im[ Omega_R * Y2_n* ]
+    a = -2 * np.imag(Omega_R * np.conj(Y2_n))
     
-    # Thành phần 1: -(i / hbar) * [e_e(t) + e_h(t)] * p_k(t)
-    term1 = -(1j / hbar) * (e_e + e_h) * p_k
+    # Trả về complex(a, a) tương đương với a + a*j trong Python
+    return a + 1j * a
+
+
+def F2_n(Y1, Y2, n_index, t, N, hbar, T2, delta_eps, delta_0):
+    """
+    Tính F_{2,n}
+    Y1: Mảng các giá trị Y_1 (có phần thực và phần ảo)
+    Y2: Mảng các giá trị Y_2 (độ phân cực)
+    delta_eps (Δε), delta_0 (Δ0): Các hằng số dải năng lượng (lấy từ const)
+    """
+    # 1. Tính En(Y) và Omega_R(Y)
+    E_n_val = En(Y1, n_index, N)
+    Omega_R = OmegaRabi(Y2, n_index, t, N)
     
-    # Thành phần 2: + i * [1 - f_{e,k}(t) - f_{h,k}(t)] * Omega_R(t)
-    term2 = 1j * (1 - f_e_k - f_h_k) * Omega_R
+    # Lấy giá trị tại n hiện tại
+    idx = n_index - 1
+    Y1_n = Y1[idx]
+    Y2_n = Y2[idx]
     
-    # Thành phần 3: - p_k(t) / T2
-    term3 = - (p_k / T2)
+    # 2. Tính toán 3 thành phần của F_{2,n}
     
-    # Tổng hợp các thành phần lại
+    # Thành phần 1: -(i / ħ) * [n*Δε - Δ0 - E_n(Y)] * Y_{2,n}
+    term1 = -(1j / hbar) * (n_index * delta_eps - delta_0 - E_n_val) * Y2_n
+    
+    # Thành phần 2: + i * [1 - Re[Y_{1,n}] - Im[Y_{1,n}]] * Ω_n^R(Y)
+    term2 = 1j * (1 - np.real(Y1_n) - np.imag(Y1_n)) * Omega_R
+    
+    # Thành phần 3: - Y_{2,n} / T2
+    term3 = - (Y2_n / T2)
+    
+    # Tổng hợp
     return term1 + term2 + term3
