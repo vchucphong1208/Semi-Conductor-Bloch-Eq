2f22d7def1acfc064a64c24b8af6170c6b13b276
Trường An <147297783+ALT-03@users.noreply.github.com>
2026-04-11 19:39:52 +0700

    Remove duplicate import statements
    Removed duplicate import statements for const and def_func.

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
 from const import *
 from def_func import *
-
-from const import *
-from def_func import *
 import numpy as np
 # Import thư viện vẽ đồ thị (nếu cần)
 import matplotlib.pyplot as plt 
